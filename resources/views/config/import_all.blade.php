ecea6211705f130e26ef3dd6165377207acbe89e
fqueta <ger.maisaqui1@gmail.com>
2025-04-14 06:37:17 -0300

    14/04/2024

--- resources/views/config/import_all.blade.php
+++ resources/views/config/import_all.blade.php
@@ -28,11 +28,7 @@
                     </div>
                 @endif
 
-<<<<<<< HEAD
-                @if(session('error'))
-=======
                 @if(session('erro'))
->>>>>>> a87a7ce6f2906f994fa9fc31c6e2a856c33f8b38
                     <div class="alert alert-danger">
                         {{ session('erro') }}
                     </div>
@@ -52,73 +48,7 @@
 
 @section('js')
     @include('qlib.jslib')
-<<<<<<< HEAD
-    {{-- <script type="text/javascript">
-        $(function(){
-            $('a.print-card').on('click',function(e){
-                openPageLink(e,$(this).attr('href'),"{{date('Y')}}");
-            });
-            $('[mask-cpf]').inputmask('999.999.999-99');
-            $('[mask-cnpj]').inputmask('99.999.999/9999-99');
-            $('[mask-data]').inputmask('99/99/9999');
-            $('[mask-cep]').inputmask('99.999-999');
-            $('#form-import').on('submit', function(e) {
-                e.preventDefault(); // Impede o envio tradicional do form
 
-                let formData = new FormData(this); // Cria objeto com os dados do form
-                let url = $('#form-import').attr('action');
-                console.log(formData);
-                $.ajax({
-                    url: url,           // sua rota no backend
-                    type: 'POST',
-                    data: formData,
-                    contentType: false,       // NÃO definir manualmente
-                    processData: false,       // NÃO transformar em query string
-                    success: function(response) {
-                    console.log('Sucesso!', response);
-                    },
-                    error: function(xhr, status, error) {
-                    console.error('Erro:', error);
-                    }
-                });
-            });
-
-        });
-    </script> --}}
-=======
-    <script type="text/javascript">
-        // $(function(){
-        //     $('a.print-card').on('click',function(e){
-        //         openPageLink(e,$(this).attr('href'),"{{date('Y')}}");
-        //     });
-        //     $('[mask-cpf]').inputmask('999.999.999-99');
-        //     $('[mask-cnpj]').inputmask('99.999.999/9999-99');
-        //     $('[mask-data]').inputmask('99/99/9999');
-        //     $('[mask-cep]').inputmask('99.999-999');
-        //     $('#form-import').on('submit', function(e) {
-        //         e.preventDefault(); // Impede o envio tradicional do form
-
-        //         let formData = new FormData(this); // Cria objeto com os dados do form
-        //         let url = $('#form-import').attr('action');
-        //         console.log(formData);
-        //         $.ajax({
-        //             url: url,           // sua rota no backend
-        //             type: 'POST',
-        //             data: formData,
-        //             contentType: false,       // NÃO definir manualmente
-        //             processData: false,       // NÃO transformar em query string
-        //             success: function(response) {
-        //             console.log('Sucesso!', response);
-        //             },
-        //             error: function(xhr, status, error) {
-        //             console.error('Erro:', error);
-        //             }
-        //         });
-        //     });
-
-        // });
-    </script>
->>>>>>> a87a7ce6f2906f994fa9fc31c6e2a856c33f8b38
     {{-- @include('clientes.js_submit') --}}
 
 @stop
